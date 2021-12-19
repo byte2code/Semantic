ed5842ecc848a1b577fabf649c70edc7ec51cffc
byte2code <drive1first@gmail.com>
2021-12-19 19:24:34 +0530

    mega

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const links = document.querySelectorAll(".links");
 const close_buttons = document.querySelectorAll(".close-btn");
 const sections = document.querySelectorAll(".popup_box");
@@ -18,24 +17,3 @@ close_buttons.forEach((cButton, i) => {
     sections[i].style.display = "none";
   });
 });
-=======
-const links = document.querySelectorAll(".links");
-const close_buttons = document.querySelectorAll(".close-btn");
-const sections = document.querySelectorAll(".popup_box");
-
-let activeLink = 0;
-
-links.forEach((link, i) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    sections[i].style.display = "flex";
-  });
-});
-
-close_buttons.forEach((cButton, i) => {
-  cButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    sections[i].style.display = "none";
-  });
-});
->>>>>>> 050c7036d2fc4eea764e16c946813cc34614b12b
